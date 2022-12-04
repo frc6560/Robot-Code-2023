7b74ab1f05b9ba4a4c218cc792f58a524614c3d9
markzakharyan <markazakharyan@gmail.com>
2022-12-03 16:45:18 -0800

    auto works w pathplanner lib. also restructured code with "citrus com." format

---
+++ src/main/java/com/team6560/frc2022/commands/auto/CommandGroupInterface.java
@@ -0,0 +1,8 @@
+package com.team6560.frc2022.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** Add your docs here. */
+public interface CommandGroupInterface {
+    public Command getCommand();
+}
