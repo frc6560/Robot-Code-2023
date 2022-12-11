15657083f7b90a0380dacd3b69e2b5b1c02975a3
markzakharyan <markazakharyan@gmail.com>
2022-12-11 12:05:43 -0800

    added javadoc to drivetrain

--- src/main/java/com/team6560/frc2023/subsystems/Drivetrain.java
+++ src/main/java/com/team6560/frc2023/subsystems/Drivetrain.java
@@ -28,85 +28,101 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Drivetrain extends SubsystemBase {
         /**
          * The maximum voltage that will be delivered to the drive motors.
-         * <p>
          * This can be reduced to cap the robot's maximum speed. Typically, this is
          * useful during initial testing of the robot.
          */
         public static final double MAX_VOLTAGE = 12.0;
 
+        /**
+         * The AHRS object used to get the current orientation of the robot.
+         */
         private final AHRS m_navx = new AHRS(Port.kMXP, (byte) 200);
 
-        // These are our modules. We initialize them in the constructor.
+        // These are our swerve modules. We initialize them in the constructor.
         private SwerveModule m_frontLeftModule;
         private SwerveModule m_frontRightModule;
         private SwerveModule m_backLeftModule;
         private SwerveModule m_backRightModule;
 
-        // X shape for defense
+        /**
+         * The default states for each module, corresponding to an X shape.
+         */
         public static final SwerveModuleState[] DEFAULT_MODULE_STATES = new SwerveModuleState[] {
-                new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(45))),
-                new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(-45))),
-                new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(-45))),
-                new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(45)))
+                        new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(45))),
+                        new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(-45))),
+                        new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(-45))),
+                        new SwerveModuleState(0.0, new Rotation2d(Math.toRadians(45)))
         };
 
+        /**
+         * The array of swerve modules that make up this drivetrain.
+         */
         public SwerveModule[] modules;
+
+        /**
+         * The odometry object for this drivetrain.
+         */
         private SwerveDriveOdometry odometry = new SwerveDriveOdometry(Constants.m_kinematics, m_navx.getRotation2d());
 
-        private double GYRO_OFFSET = 360 / (360-12);
+        /**
+         * The offset to apply to the gyroscope readings to account for any drift.
+         */
+        private double GYRO_OFFSET = 360 / (360 - 12);
 
-
+        /**
+         * Constructs a new `Drivetrain` object and initializes the swerve modules.
+         */
         public Drivetrain() {
                 ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
 
                 NtValueDisplay.ntDispTab("Drivetrain")
-                        .add("Yaw", () -> this.getGyroscopeRotation().getDegrees())
-                        .add("Continuous Yaw", () -> m_navx.getRotation2d().getDegrees()*GYRO_OFFSET)
-                        ;
+                                .add("Yaw", () -> this.getGyroscopeRotation().getDegrees())
+                                .add("Continuous Yaw", () -> m_navx.getRotation2d().getDegrees() * GYRO_OFFSET);
 
                 m_frontLeftModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
-                        // This parameter is optional, but will allow you to see the current state of
-                        // the module on the dashboard.
-                        tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-                                .withSize(2, 4)
-                                .withPosition(0, 0),
-                        Mk4iSwerveModuleHelper.GearRatio.L2,
-                        FRONT_LEFT_MODULE_DRIVE_MOTOR,
-                        FRONT_LEFT_MODULE_STEER_MOTOR,
-                        FRONT_LEFT_MODULE_STEER_ENCODER,
-                        FRONT_LEFT_MODULE_STEER_OFFSET);
+                                // This parameter is optional, but will allow you to see the current state of
+                                // the module on the dashboard.
+                                tab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(0, 0),
+                                Mk4iSwerveModuleHelper.GearRatio.L2,
+                                FRONT_LEFT_MODULE_DRIVE_MOTOR,
+                                FRONT_LEFT_MODULE_STEER_MOTOR,
+                                FRONT_LEFT_MODULE_STEER_ENCODER,
+                                FRONT_LEFT_MODULE_STEER_OFFSET);
 
                 m_frontRightModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
-                        tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-                                .withSize(2, 4)
-                                .withPosition(2, 0),
-                        Mk4iSwerveModuleHelper.GearRatio.L2,
-                        FRONT_RIGHT_MODULE_DRIVE_MOTOR,
-                        FRONT_RIGHT_MODULE_STEER_MOTOR,
-                        FRONT_RIGHT_MODULE_STEER_ENCODER,
-                        FRONT_RIGHT_MODULE_STEER_OFFSET);
+                                tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(2, 0),
+                                Mk4iSwerveModuleHelper.GearRatio.L2,
+                                FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+                                FRONT_RIGHT_MODULE_STEER_MOTOR,
+                                FRONT_RIGHT_MODULE_STEER_ENCODER,
+                                FRONT_RIGHT_MODULE_STEER_OFFSET);
 
                 m_backLeftModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
-                        tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-                                .withSize(2, 4)
-                                .withPosition(4, 0),
-                        Mk4iSwerveModuleHelper.GearRatio.L2,
-                        BACK_LEFT_MODULE_DRIVE_MOTOR,
-                        BACK_LEFT_MODULE_STEER_MOTOR,
-                        BACK_LEFT_MODULE_STEER_ENCODER,
-                        BACK_LEFT_MODULE_STEER_OFFSET);
+                                tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(4, 0),
+                                Mk4iSwerveModuleHelper.GearRatio.L2,
+                                BACK_LEFT_MODULE_DRIVE_MOTOR,
+                                BACK_LEFT_MODULE_STEER_MOTOR,
+                                BACK_LEFT_MODULE_STEER_ENCODER,
+                                BACK_LEFT_MODULE_STEER_OFFSET);
 
                 m_backRightModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
-                        tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-                                .withSize(2, 4)
-                                .withPosition(6, 0),
-                        Mk4iSwerveModuleHelper.GearRatio.L2,
-                        BACK_RIGHT_MODULE_DRIVE_MOTOR,
-                        BACK_RIGHT_MODULE_STEER_MOTOR,
-                        BACK_RIGHT_MODULE_STEER_ENCODER,
-                        BACK_RIGHT_MODULE_STEER_OFFSET);
+                                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(6, 0),
+                                Mk4iSwerveModuleHelper.GearRatio.L2,
+                                BACK_RIGHT_MODULE_DRIVE_MOTOR,
+                                BACK_RIGHT_MODULE_STEER_MOTOR,
+                                BACK_RIGHT_MODULE_STEER_ENCODER,
+                                BACK_RIGHT_MODULE_STEER_OFFSET);
 
-                modules = new SwerveModule[] {m_frontLeftModule, m_frontRightModule, m_backLeftModule, m_backRightModule};
+                modules = new SwerveModule[] { m_frontLeftModule, m_frontRightModule, m_backLeftModule,
+                                m_backRightModule };
         }
 
         /**
@@ -118,22 +134,36 @@ public class Drivetrain extends SubsystemBase {
                 m_navx.zeroYaw();
         }
 
+        /**
+         * Gets the current rotation of the robot according to the gyroscope.
+         *
+         * @return The rotation (yaw) of the robot
+         **/
         public Rotation2d getGyroscopeRotation() {
                 // if (m_navx.isMagnetometerCalibrated()) {
-                //         // We will only get valid fused headings if the magnetometer is calibrated
-                //         return Rotation2d.fromDegrees(m_navx.getFusedHeading());
+                // // We will only get valid fused headings if the magnetometer is calibrated
+                // return Rotation2d.fromDegrees(m_navx.getFusedHeading());
                 // }
                 // We will only get valid fused headings if the magnetometer is calibrated
                 // We have to invert the angle of the NavX so that rotating the robot
                 // counter-clockwise makes the angle increase.
-                return Rotation2d.fromDegrees(360.0 - m_navx.getYaw() * GYRO_OFFSET);
+                return Rotation2d.fromDegrees(360.0 - (m_navx.getYaw() * GYRO_OFFSET));
         }
 
+        /**
+         * 
+         * This method is used to control the movement of the chassis.
+         * 
+         * @param chassisSpeeds an object containing the desired speeds of the chassis
+         *                      in the X and Y directions, as well as the desired
+         *                      rotational speed
+         */
         public void drive(ChassisSpeeds chassisSpeeds) {
                 // X shape for defense
                 SwerveModuleState[] states = DEFAULT_MODULE_STATES;
 
-                if (chassisSpeeds.vxMetersPerSecond != 0 || chassisSpeeds.vyMetersPerSecond != 0 || chassisSpeeds.omegaRadiansPerSecond != 0) {
+                if (chassisSpeeds.vxMetersPerSecond != 0 || chassisSpeeds.vyMetersPerSecond != 0
+                                || chassisSpeeds.omegaRadiansPerSecond != 0) {
                         states = Constants.m_kinematics.toSwerveModuleStates(chassisSpeeds);
                         SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
                         setChassisState(states);
@@ -141,13 +171,13 @@ public class Drivetrain extends SubsystemBase {
                 }
 
                 for (int i = 0; i < 4; i++) {
-                        if (Math.abs(modules[i].getSteerAngle() - DEFAULT_MODULE_STATES[i].angle.getDegrees()) < 5.0 || modules[i].getDriveVelocity() != 0.0) {
+                        if (Math.abs(modules[i].getSteerAngle() - DEFAULT_MODULE_STATES[i].angle.getDegrees()) < 5.0
+                                        || modules[i].getDriveVelocity() != 0.0) {
                                 setChassisState(DEFAULT_MODULE_STATES);
                                 return;
                         }
                 }
 
-                
         }
 
         @Override
@@ -155,23 +185,48 @@ public class Drivetrain extends SubsystemBase {
 
         }
 
+        /**
+         * Sets the speeds and orientations of each swerve module.
+         * array order: front left, front right, back left, back right
+         *
+         * @param moduleStates The desired states for each module.
+         */
         public void setChassisState(SwerveModuleState[] states) {
-                m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
-                m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
-                m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
-                m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+                m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[0].angle.getRadians());
+                m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[1].angle.getRadians());
+                m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[2].angle.getRadians());
+                m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[3].angle.getRadians());
 
                 odometry.update(m_navx.getRotation2d(), states);
         }
 
+        /**
+         * Gets the current pose of the robot according to the odometry calculations.
+         *
+         * @return The pose of the robot.
+         */
         public Pose2d getPose() {
                 return odometry.getPoseMeters();
         }
 
+        /**
+         * 
+         * This method is used to reset the position of the robot's odometry.
+         * 
+         * @param pose the new pose to use as the starting position for the odometry
+         */
         public void resetOdometry(Pose2d pose) {
                 odometry.resetPosition(pose, getGyroscopeRotation());
         }
 
+        /**
+         * 
+         * This method is used to stop all of the swerve drive modules.
+         */
         public void stopModules() {
                 setChassisState(DEFAULT_MODULE_STATES);
         }
